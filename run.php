456cc2670df4c7eb6b01909a23277447a57339c6
Adrien Gallou <adriengallou@gmail.com>
2017-08-27 22:04:38 +0200

    ajout notifications slack

--- run.php
+++ run.php
@@ -1,5 +1,40 @@
 <?php
 
+function slackNotification($text, array $links = []) {
+  $payload = [
+    'channel' => '#test_depoy3',
+    'text' => $text,
+  ];
+
+  $slackToken = getenv('DEPLOY_SLACK_TOKEN');
+
+  foreach ($links as $title => $href) {
+    $payload['attachments'][] = [
+      "title_link" => $href,
+      "text" => $title,
+    ];
+  }
+
+  $url = "https://hooks.slack.com/services/";
+
+  $jsonPayload = json_encode($payload);
+  $curl = curl_init($url . $slackToken);
+  curl_setopt_array($curl, [
+      CURLOPT_RETURNTRANSFER => 1,
+      CURLOPT_POSTFIELDS => http_build_query(['payload' => $jsonPayload]),
+  ]);
+
+  $result = curl_exec($curl);
+
+  if ($result != 'ok') {
+    throw new \RuntimeException('Notification slack impossible : ' . var_export($result, true));
+  }
+}
+
+$projects = [
+  'web' => ['Voir event.afup.org' => 'https://event.afup.org'],
+];
+
 if (!isset($argv[1])) {
   throw new \InvalidArgumentException('Undefined project name');
 }
@@ -30,6 +65,9 @@ $playbook = escapeshellarg(__DIR__ . '/playbooks/' . $project . '.yml');
 
 $command = 'ANSIBLE_LOCAL_TEMP=/tmp/ansible_local_tmp_deploy ANSIBLE_REMOTE_TEMP=/tmp/ansible_remote_tmp_deploy /usr/local/bin/ansible-playbook  -i "localhost," -c local -vvv ' . $playbook;
 
+
+slackNotification(sprintf('Le déploiement du projet %s a débuté', $project));
+
 $output = [];
 $return = 0;
 exec($command, $output, $return);
@@ -48,4 +86,5 @@ if (!unlink($lockFile)) {
   throw new \RuntimeException('Erreur suppression du fichier de lock ' . $lockFile);
 }
 
+slackNotification(sprintf('Le projet %s a été mis à jour', $project), isset($projects[$project]) ? $projects[$project] : []);
 
